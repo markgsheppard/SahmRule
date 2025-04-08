e7084487a3397ce9dde318af7c29ea9a7915d2fa
giorgi-ghviniashvili <mr.g.ghv@gmail.com>
2025-04-07 17:20:31 -0700

    update map

--- counties-viz/counties-viz.js
+++ counties-viz/counties-viz.js
@@ -9,17 +9,20 @@ class CountiesViz {
 			last_sahm_value: {
 				domain: [0.25, 0.5, 0.75, 1, 1.25, 1.5],
 				range: d3.schemeBlues[9].slice(2, 9),
-				tickFormat: d => d
-			},
-			committee_lead_time: {
-				domain: [50, 100, 125],
-				range: d3.schemeBlues[6].slice(2, 6),
-				tickFormat: d => Math.round(d)
+				tickFormat: d => d,
+				label: 'Outlook'
 			},
 			accuracy: {
 				domain: [0, 5, 10, 15, 20],
 				range: d3.schemeBlues[6],
+				label: 'Accuracy',
 				tickFormat: d => Math.round(d)
+			},
+			committee_lead_time: {
+				domain: [50, 75, 100, 125],
+				range: d3.schemeBlues[6].slice(1, 6),
+				tickFormat: d => Math.round(d),
+				label: 'Advance'
 			}
 		}
 
@@ -34,7 +37,12 @@ class CountiesViz {
 
 		const countiesFeatures = topojson.feature(usTopo, usTopo.objects.counties)
 		const statesFeatures = topojson.feature(usTopo, usTopo.objects.states)
-		const countiesData = new Map(counties.map(d => [d.county.trim(), d]))
+		const statemap = new Map(statesFeatures.features.map(d => [d.id, d]))
+		const countiesData = new Map(
+			counties.map(d => {
+				return [this.normalizeCountyName(d.county), d]
+			})
+		)
 		const accuracyExtent = d3.extent(counties, d => d.accuracy)
 		const last_sahm_value_extent = d3.extent(counties, d => d.last_sahm_value)
 		const committee_lead_time_extent = d3.extent(
@@ -46,6 +54,7 @@ class CountiesViz {
 			countiesFeatures,
 			statesFeatures,
 			countiesData,
+			statemap,
 			metricDomains: {
 				accuracy: accuracyExtent,
 				last_sahm_value: [0, last_sahm_value_extent[1]],
@@ -78,7 +87,7 @@ class CountiesViz {
 				type: 'threshold',
 				domain: this.metricsConfig[this.metric].domain,
 				range: this.metricsConfig[this.metric].range,
-				label: this.metric,
+				label: this.metricsConfig[this.metric].label,
 				legend: true,
 				tickFormat: this.metricsConfig[this.metric].tickFormat
 			},
@@ -95,36 +104,38 @@ class CountiesViz {
 						},
 						tip: true,
 						channels: {
-							County: d => d.properties.name
-							// State: d => statemap.get(d.id.slice(0, 2)).properties.name
+							County: d => d.properties.name,
+							State: d =>
+								this.dataset.statemap.get(d.id.slice(0, 2)).properties.name
 						}
 					})
 				),
 				Plot.geo(this.dataset.statesFeatures, { stroke: 'white' })
-				// Plot.dot(
-				// 	this.dataset.countiesFeatures,
-				// 	Plot.centroid({
-				// 		filter: d => d.properties.name.match(/^V/),
-				// 		fill: 'currentColor',
-				// 		stroke: 'white'
-				// 	})
-				// ),
-				// Plot.text(
-				// 	this.dataset.countiesFeatures,
-				// 	Plot.centroid({
-				// 		filter: d => d.properties.name.match(/^V/),
-				// 		text: d => d.properties.name,
-				// 		fill: 'currentColor',
-				// 		stroke: 'white',
-				// 		textAnchor: 'start',
-				// 		dx: 6
-				// 	})
-				// )
 			]
 		})
 		const div = document.querySelector('#counties-viz')
 		div.innerHTML = ''
 		div.append(map)
+	}
+
+	normalizeCountyName(countyName) {
+		const suffixes = [
+			' County',
+			' city',
+			' Borough',
+			' Parish',
+			' Municipality',
+			' Town',
+			' Village',
+			' Census Area',
+			' City and Borough'
+		]
+
+		let normalized = countyName
+		for (const suffix of suffixes) {
+			normalized = normalized.replace(new RegExp(`${suffix}$`), '')
+		}
+		return normalized.trim()
 	}
 }
 
